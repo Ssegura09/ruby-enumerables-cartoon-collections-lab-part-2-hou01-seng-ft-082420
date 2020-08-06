30a76a41ac8fe091fb4bb1e0bc4c5dfbc0beac2e
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-06 23:38:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ end
   # Return a new array of the results
 def summon_captain_planet(planeteer_calls)
   #map{ |num| num * 2 }
-  planeteer_calls
+  planeteer_calls.map{ |element|
+    
+  }
 
 end
 
