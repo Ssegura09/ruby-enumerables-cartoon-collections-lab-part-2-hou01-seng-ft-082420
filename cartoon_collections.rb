e27ad836c4e6621231c02b3db2fc7da15e4696e4
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-06 23:53:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do {|element| element.any?}
+  planeteer_calls.any? do |element|
     puts "#{element.capitalize}!"
 end
 end
