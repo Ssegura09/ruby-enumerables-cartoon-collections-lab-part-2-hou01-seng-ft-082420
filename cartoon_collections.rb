ee0c5e4556541bdbaeb996a3010de9842b8f6088
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-06 23:35:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+ require 'pry'
  # square_array: Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
 def square_array(array)
@@ -6,10 +7,11 @@ def square_array(array)
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-  
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+def summon_captain_planet(planeteer_calls)
+  binding.pry
+
 end
 
 def long_planeteer_calls(planeteer_calls)
